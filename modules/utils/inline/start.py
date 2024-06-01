01a0987c18ee8053aff4fd095103eba183aabefb
AnshNaagar <158273761+AnshNaagar@users.noreply.github.com>
2024-06-01 15:38:45 +0530

    Update start.py

--- modules/utils/inline/start.py
+++ modules/utils/inline/start.py
@@ -21,11 +21,11 @@ def start_pannel(_):
         [
             InlineKeyboardButton(
                 text="📡 𝐔𝐩𝐝𝐚𝐭𝐞𝐬",
-                url=f"https://t.me/BikashGedgetsTech",
+                url=f"https://t.me/xd_olivia",
             ),
             InlineKeyboardButton(
                 text="𝐒𝐮𝐩𝐩𝐨𝐫𝐭 💬",
-                url=f"https://t.me/bgt_chat",
+                url=f"https://t.me/oliviaxfam",
             )
         ],
         [
@@ -48,10 +48,10 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
         [
             InlineKeyboardButton(
                 text="📡 𝐔𝐩𝐝𝐚𝐭𝐞𝐬",
-                url=f"https://t.me/BikashGedgetsTech"),
+                url=f"https://t.me/xd_olivia"),
             InlineKeyboardButton(
                 text="𝐒𝐮𝐩𝐩𝐨𝐫𝐭 💬",
-                url=f"https://t.me/bgt_chat")
+                url=f"https://t.me/oliviaxfam")
         ],
         [
             InlineKeyboardButton(
